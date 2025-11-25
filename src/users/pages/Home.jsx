a83b08df21a8971aed67476edfe0eef49e36372a
soniagkalliath <soniageorge111@gmail.com>
2025-11-25 12:58:07 +0530

    header design

--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import { FaSearch } from 'react-icons/fa'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Header/>
+    <div>
+      {/* landing part - search */}
+      <div style={{height:'500px'}}  className='flex justify-center items-center flex-col bg-[url(/bg.jpg)] bg-cover bg-center text-white'>
+        <div style={{height:'500px',backgroundColor:'rgba(0,0,0,0.5)'}}  className='w-full flex justify-center items-center flex-col '>
+          <h1 className="text-5xl font-bold mb-2">Wonderful Gifts</h1>
+          <p>Gift your family and friends a book</p>
+          {/* seach */}
+          <div className="mt-9 flex items-center">
+            <input type="text" className="bg-white rounded-3xl text-black w-100 placeholder-gray-500 p-2" placeholder='Search Books Here'/>
+            <button className='text-gray-500 ' style={{marginLeft:'-40px'}}><FaSearch /></button>
+          </div>
+        </div>
+      </div>
+      {/* new arroval */}
+      <section className='md:px-40 p-5  my-5 flex flex-col justify-center items-center'>
+        <h1 className="text-3xl font-bold">NEW ARRIVALS</h1>
+        <h2 className="text-2xl">Explore Our Latest Collection</h2>
+        {/* books row & col */}
+      </section>  
+      {/* author */}
+      {/* testimony */}
+    </div>
+    <Footer/>
+    </>
   )
 }
 
